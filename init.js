af12fde4b774e4dbf336c3495b7493428438fa6f
diepgemskyworld <84183305+diepgemskyworld@users.noreply.github.com>
2021-05-14 19:05:39 +0700

    Add files via upload

---
+++ init.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRTmY3FO_jKqO7pVJXcMQduOHaYk4C-gM",
+    authDomain: "diep-gemskyworld.firebaseapp.com",
+    projectId: "diep-gemskyworld",
+    storageBucket: "diep-gemskyworld.appspot.com",
+    messagingSenderId: "887953951081",
+    appId: "1:887953951081:web:c9a165fc5fa25b105dd5f8",
+    measurementId: "G-GC5F188DW9"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+$(document).ready(function(){
+    alert('0k')
+})
